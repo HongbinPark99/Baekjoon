8cebb9543205f5d39e3a2406088cb6934e375d42
hongbinpark99 <hongbin542@gmail.com>
2021-12-25 12:12:29 +0900

    second commit

---
+++ 1차원 배열/이오육이.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class 이오육이 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        
+
+        int[] input = new int[9];
+        int max = 0;
+        int cnt = 0;
+        for (int i = 0; i < 9; i++) {
+            input[i] = in.nextInt();
+        }
+        for (int i = 0; i < 9; i++) {
+            if (input[i] > max) {
+                max = input[i];
+                cnt = i + 1;
+            }
+        }
+        System.out.println(max);
+        System.out.println(cnt);
+    }
+}
