60c2425fecdfbf0b35ffc715b9855530ee57b700
hongbinpark99 <hongbin542@gmail.com>
2022-01-04 22:22:53 +0900

    second commit

---
+++ 문자열/일일육오사.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class 일일육오사{
+    public static void main(String[] args) {
+        
+        Scanner in =new Scanner(System.in);
+        
+
+        int ch = in.next().charAt(0);
+
+        System.out.print(ch);
+        in.close();
+    }
+}
